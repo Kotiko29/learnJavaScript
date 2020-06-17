40a03b3ca3ff9a1d46f93f91bbdca70429e853ad
Kotiko29 <kotiko29@yandex.ru>
2020-06-17 13:26:29 +0500

    Higher-order function

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 import style from './src/assets/style/index.styl'; // импорт стилей
+
+// Функции высшего порядка
+
+const arr = ['Masha', 'Vlad', 'Olga','Ekaterina', 'Max', 'Alexey'];
+
+function mapArray(arr, fn) {
+  const res = [];
+
+  for(let item in arr) {
+    res.push(fn(arr[item]));
+  }
+  return res; 
+}
+
+function nameLangth(el) {
+  return el.length;
+}
+let res1 = mapArray(arr, nameLangth);
+
+console.log(res1);
+
+function nametoUpperCase(el) {
+  return el.toUpperCase();
+}
+
+let res2 = mapArray(arr, nametoUpperCase);
+console.log(res2);
+
