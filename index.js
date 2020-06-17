9703b823c518f9298f44bd70224b890b1ef3c06e
Kotiko29 <kotiko29@yandex.ru>
2020-06-17 14:18:52 +0500

    creating a dictionary in an object

--- index.js
+++ index.js
@@ -18,12 +18,37 @@ function nameLangth(el) {
 }
 let res1 = mapArray(arr, nameLangth);
 
-console.log(res1);
+// console.log(res1);
 
 function nametoUpperCase(el) {
   return el.toUpperCase();
 }
 
 let res2 = mapArray(arr, nametoUpperCase);
-console.log(res2);
+// console.log(res2);
 
+function greeting(firstName) {
+  return function(lastName){
+    return `Hello ${firstName} ${lastName}`;
+  };
+}
+// let testGreeting = greeting('Ivan');
+// console.log(testGreeting);
+// let fullName = testGreeting('Ivanov');
+
+const fullName = greeting('Ivan')('Ivanov');
+// console.log(fullName);
+
+function question(job) {
+  const jobDictionary = {
+    developer: 'что такое JS?',
+    teacher: 'какой предмет вы ведете?',
+    driver: 'какой у вас стаж?'
+  };
+  return function(name) {
+    return `${name}, ${jobDictionary[job]}`;
+  };
+}
+
+const answer = question('driver')('Igor');
+console.log(answer);
