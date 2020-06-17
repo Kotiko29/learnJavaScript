4aeeebd3bb318b42b8e5a987d7741797e60f509b
Kotiko29 <kotiko29@yandex.ru>
2020-06-17 15:04:51 +0500

    Higher-order function__homework is done

--- index.js
+++ index.js
@@ -51,4 +51,37 @@ function question(job) {
 }
 
 const answer = question('driver')('Igor');
-console.log(answer);
+// console.log(answer);
+
+// Домашнее задание по функциям высшего порядка
+
+function firstFunc(arr, fn) {
+  let res = '';
+  for(let item in arr) {
+    res += (fn(arr[item]));
+  }
+  return `New value: ${res}`;
+}
+
+function handler1(el) {
+  return el[0].toUpperCase() + el.slice(1);
+}
+
+console.log(firstFunc(['my', 'name', 'is', 'Trinity'], handler1));
+
+function handler2(el) {
+  return `${el*10}, `;
+}
+console.log(firstFunc([10, 20, 30], handler2) );
+
+function handler3(el) {
+  return `${el.name} is ${el.age}, `;
+}
+
+console.log(firstFunc([{age: 45, name: 'Jhon'}, {age: 20, name: 'Aaron'}], handler3));
+
+function handler4(el) {
+  return `${el.split("").reverse().join("")}, `;
+}
+
+console.log(firstFunc(['abs', '123'], handler4));
